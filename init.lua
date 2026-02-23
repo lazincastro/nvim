ed7b3c31b249e4d81d2950d76cde89151cdca4ca
Lazaro Castro <lazaro.castro@gmail.com>
2026-02-23 20:55:50 -0300

    Init commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Bootstrap mini.nvim
+require("config.bootstrap")
+
+-- Core config
+require("config.options")
+-- require("config.theme")
+require("config.keymaps")
+require("config.highlights")
+require("config.autocommands")
+
+-- Plugins
+require("plugins.mini")
+require("plugins.nvimtree")
+require("plugins.copilot")
